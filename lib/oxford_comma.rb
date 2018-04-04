c171ad3c9f934a54361154a95d24cb1393455442
Luis Rodriguez <zacha@tutanota.com>
2018-04-04 22:16:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  last_element = array.pop()
-  string_array = array.join(", ")
-  return string_array + ", and " + last_element
+  if array.size == 1
+    return array[0]
+  else
+    last_element = array.pop()
+    string_array = array.join(", ")
+    return string_array + ", and " + last_element
+  end
 end
